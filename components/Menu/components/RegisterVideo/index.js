20e2d0a4f89d3f5f5377bbc256bae7ec9c4921ec
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-11-14 12:08:08 -0300

    utilizando UseState

--- components/Menu/components/RegisterVideo/index.js
+++ components/Menu/components/RegisterVideo/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
+// Whiteboarding
+// Custom Hook
+function useForm(propsDoForm) {
+  const [values, setValues] = React.useState(propsDoForm.initialValues);
+
+  return {
+    values,
+    handleChange: (evento) => {
+      console.log(evento.target);
+      const value = evento.target.value;
+      const name = evento.target.name;
+      setValues({
+        ...values,
+        [name]: value,
+      });
+    },
+    clearForm() {
+      setValues({});
+    },
+  };
+}
 export default function RegisterVideo() {
   const formCadastro = useForm({
     initialValues: { titulo: "Frost punk", url: "https://youtube.." },
